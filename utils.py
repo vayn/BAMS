6fb1f4f00e5725659c5c133114b283fc94364f65
Vincent Tsai <vayn@vayn.de>
2013-08-25 23:32:06 +0800

    Update utils

---
+++ utils.py
@@ -0,0 +1,27 @@
+# vim: set fileencoding=utf-8:
+import os
+import ctypes
+import _winreg as winreg
+
+
+__author__ = "Vayn a.k.a. VT <vayn@vayn.de>"
+__version__ = '0.6.0'
+
+
+def setappid():
+  u"""设置 AppID
+  
+  可以避免因与 Python 的 AppID 相同而在任务栏被折叠
+  注意：仅在 Windows XP 以上版本有效
+  
+  """
+  try:
+    myappid = "VT.BAMS.%s" % __version__
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+  except AttributeError:
+    pass
+
+def get_desktop():
+  key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,\
+      r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
+  return winreg.QueryValueEx(key, 'Desktop')[0]
